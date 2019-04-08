a077b2d400ec956c8887a6b93f8e86f293fcc5cf
Ryan Braid <rbraid@mines.edu>
2019-04-08 10:22:43 -0400

    Made ring functions robust against not finding the histogram, which is now possible due to allowing side detectors

--- src/Rings.cxx
+++ src/Rings.cxx
@@ -4,7 +4,10 @@ int RingNumber(int stripX, int stripY, int detector, TFile* ringFile)
 {  
   TH2D* histo = (TH2D*)ringFile->Get(Form("Total_Rings_0_d%i_pid_Be10",detector));
   int binNo = histo->GetBin(stripX+1,stripY+1);
-  int Ring = histo->GetBinContent(binNo);
+  int Ring = -1;
+  if(histo)
+    Ring = histo->GetBinContent(binNo);
+  
   Ring -= 1;
   
   return Ring;
@@ -18,7 +21,10 @@ int RingNumber(TCSMHit* Hit, TFile* ringFile)
 double RingSA(int Ring, int Det, TFile* ringFile)
 {  
   TH1D* spec = (TH1D*)ringFile->Get(Form("SA_0_d%i_pid",Det));
-  double TotalSolidAngle = spec->GetBinContent(Ring+1);
+  double TotalSolidAngle = -2;
+  
+  if(spec)
+    TotalSolidAngle = spec->GetBinContent(Ring+1);
   
   return TotalSolidAngle;
 }
@@ -26,7 +32,10 @@ double RingSA(int Ring, int Det, TFile* ringFile)
 double RingSA_err(int Ring, int Det, TFile* ringFile)
 {  
   TH1D* spec = (TH1D*)ringFile->Get(Form("SA_0_d%i_pid_err",Det));
-  double TotalSolidAngle_err = spec->GetBinContent(Ring+1);
+  double TotalSolidAngle_err = -2;
+  
+  if(spec)
+spec->GetBinContent(Ring+1);
   
   return TotalSolidAngle_err;
 }
