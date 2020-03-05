9c08f0fb1eebd776351b5ed5e9304bf8bb3249c5
Ryan Braid <rbraid@mines.edu>
2020-03-05 13:23:25 -0700

    Who needs gamma eff? It is an expensive plot.

--- src/GammaMakers.cxx
+++ src/GammaMakers.cxx
@@ -4,31 +4,31 @@ void ProcessGammas(TChain* chain,TList* outlist,TList* suppList)
 {
   TStopwatch w;
   w.Start();
-  
+
   TTigress *tigress =  new TTigress;
   chain->SetBranchAddress("TTigress",&tigress);
-  
+
   TFile* gammaFile = (TFile*) suppList->FindObject("inputRootFiles/GammaInfo.root");
-  
+
   int nentries = chain->GetEntries();
   for(int x=0; x<nentries; x++)
   {
     chain->GetEntry(x);
-    
+
     if(tigress->GetAddBackMultiplicity() == 0)
       continue;
-    
+
     for(int y=0; y<tigress->GetAddBackMultiplicity();y++)
     {
       TTigressHit *hit = tigress->GetAddBackHit(y);
       TH1D* temp = 0;
-      
+
       temp = (TH1D*)outlist->FindObject("GammaSum");
       temp->Fill(hit->GetCore()->GetEnergy()/1000.);
-      if(hit->GetCore()->GetEnergy() > 10 && hit->GetCore()->GetEnergy() < 3000){
-        temp = (TH1D*)outlist->FindObject("GammaSum_eff");
-        temp->Fill(hit->GetCore()->GetEnergy()/1000.,EfficiencyWeight(hit,gammaFile));
-      }
+      // if(hit->GetCore()->GetEnergy() > 10 && hit->GetCore()->GetEnergy() < 3000){
+      //   temp = (TH1D*)outlist->FindObject("GammaSum_eff");
+      //   temp->Fill(hit->GetCore()->GetEnergy()/1000.,EfficiencyWeight(hit,gammaFile));
+      // }
     }
     if(x%200000==0)
     {
